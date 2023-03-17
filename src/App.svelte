149114b90dc4c6ef8a01a98fd88d7d2380585b72
annazhurba <anna0zhurba@gmail.com>
2023-03-17 23:40:07 +0100

    New files + test dialog window

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,12 @@
 <script>
-	export let name;
+	import Buttons from "./Buttons.svelte";
+	let state = 0;
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>Draw, I Guess</h1>
 </main>
+<Buttons/>
 
 <style>
 	main {
@@ -17,7 +18,6 @@
 
 	h1 {
 		color: #ff3e00;
-		text-transform: uppercase;
 		font-size: 4em;
 		font-weight: 100;
 	}
---
+++ src/Buttons.svelte
@@ -0,0 +1,24 @@
+<script>
+    function handleHost(){
+        
+    }
+    import HostDialog from "./HostDialog.svelte";
+    let showModal = false;
+</script>
+
+<div id="buttons">
+    <button type="button" class="button">Join existing room</button>
+    <button type="button" class="button" on:click={() => (showModal = true)}>Create new room</button>
+</div>
+
+<HostDialog bind:showModal>
+    <h2 slot="header">Create new room</h2>
+</HostDialog>
+
+<style>
+    #buttons { 
+        align-items: center;
+		max-width: 300px;
+		margin: 0 auto;
+    }
+</style>
