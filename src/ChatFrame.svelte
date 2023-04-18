88cc166fcbe2ae6d952ca8557a8a66347dc71e2b
tpalinski <tpalinski@icloud.com>
2023-04-18 18:49:49 +0200

    Add chat messages display component

---
+++ src/ChatFrame.svelte
@@ -0,0 +1,14 @@
+<script>
+	var messages = [{author: "Test", content: "Some stuff"}];
+
+</script>
+
+<div id="Messages">
+	{#each messages as message}
+		<div id="message">
+		<h2>{message.author}</h2>
+		<p>{message.content}</p>
+		</div>
+	{/each}
+
+</div>
