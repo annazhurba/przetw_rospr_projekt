f9e13dc618db6ecd725be00a4bb2411d488196ad
tpalinski <tpalinski@icloud.com>
2023-04-18 19:00:23 +0200

    Add sample styling

--- src/ChatFrame.svelte
+++ src/ChatFrame.svelte
@@ -1,14 +1,28 @@
 <script>
-	var messages = [{author: "Test", content: "Some stuff"}];
+	var messages = [{author: "Test", content: "Some stuff"},
+	{author: "Another", content: "Cool story"}];
 
 </script>
 
 <div id="Messages">
 	{#each messages as message}
-		<div id="message">
+		<div class="message">
 		<h2>{message.author}</h2>
 		<p>{message.content}</p>
 		</div>
 	{/each}
 
 </div>
+
+<style>
+    .message {
+	text-align: center;
+	background: rgba(100, 100, 100, 0.5);
+	padding: 0.5rem;
+	border-radius: 1rem;
+    }
+
+    .message:nth-child(2n){
+	background: rgba(30, 30, 30, 0.5);
+    }
+</style>
